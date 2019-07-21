6c64dbb5d65f29553ef9e2af1b17124548aa0a64
Andy Tang <andyattang@sina.com>
2019-07-21 23:13:22 +0800

    test image&http

--- src/url/url.go
+++ src/url/url.go
@@ -9,18 +9,22 @@ import (
 
 func Main() {
 	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			continue
-		}
-		b, err := ioutil.ReadAll(resp.Body)
-		defer resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch loading body: %v\n", err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("%s", b)
+		checkUrl(url)
 	}
+}
+
+func checkUrl(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		return
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch loading body: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s", b)
 }
